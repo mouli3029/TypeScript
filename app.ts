d684bf884f22a108c9ba13e36548a10203d5da6b
Mouli <venkatmouli823@gmail.com>
2021-06-14 20:08:28 +0530

    Core Types

--- app.ts
+++ app.ts
@@ -1,1 +1,19 @@
-console.log("Your code goees here..")
+function add(n1:number,n2:number,showResult:boolean,phrase: string){
+    const result = n1 + n2;
+    if(showResult){
+        console.log(phrase + result);
+    }
+    else{
+        return result;
+    }
+}
+
+// No Type assignment here because Type inference (built in feature)
+// It understands what type is stored and assign the type automatically;
+let number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is: ";
+
+
+add(number1,number2,printResult,resultPhrase);
