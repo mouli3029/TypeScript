449c1acefce08352dfbd797b93775c41bc8033d7
Mouli <venkatmouli823@gmail.com>
2021-06-14 22:33:47 +0530

    Literal Types

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
-/* const ADMIN = 0;
-const READ_ONLY = 1;
-const AUTHOR = 2; */
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-;
-var person = {
-    name: "Venkata Mouli",
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
-};
-if (person.role === Role.AUTHOR) {
-    console.log('is author');
+function combine(input1, input2, resultCovertion) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    if (resultCovertion === 'as-number') {
+        return +result;
+    }
+    else {
+        return result.toString();
+    }
 }
-var favouriteActivities;
-favouriteActivities = ['Sports'];
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
-}
-console.log(person.name);
+var combineAges = combine(30, 26, 'as-number');
+console.log(combineAges);
+var combineStringAges = combine(30, 26, 'as-number');
+console.log(combineStringAges);
+var combinedNames = combine('Max', 'Anna', 'as-text');
+console.log(combinedNames);
+// Inorder to access both numbers as well as Strings we use union type
--- app.ts
+++ app.ts
@@ -1,14 +1,27 @@
-function combine(input1:number | string ,input2:number | string){
+// Any other value other than 'as-number','as-text' is not allowed . This is literal types.
+function combine(input1:number | string ,input2:number | string,resultCovertion: 'as-number' | 'as-text') {
     let result;
-    if(typeof input1 === 'number' && typeof input2 === 'number'){
-        result = input1 + input2;
+    if(typeof input1 === 'number' && typeof input2 === 'number' || resultCovertion === "as-number"){
+        result = +input1 + +input2;
     }
-    return result.toString() + input1.toString();
+    else{
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+    /* if (resultCovertion === 'as-number') {
+        return +result;
+    } else{
+        return result.toString();
+    } */
 }
 
-const combineAges = combine(30,26);
+const combineAges = combine(30,26,'as-number');
 console.log(combineAges);
 
-const combinedNames  = combine('Max','Anna');
+
+const combineStringAges = combine(30,26,'as-text');
+console.log(combineStringAges);
+
+const combinedNames  = combine('Max','Anna','as-text');
 console.log(combinedNames)
 // Inorder to access both numbers as well as Strings we use union type
